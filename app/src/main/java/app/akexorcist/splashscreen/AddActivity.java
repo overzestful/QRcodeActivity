fb352de37875c8770ddbe2c3243bd805a2e61b9c
Overzestful-Fhon <over_zestful@hotmail.co.th>
2015-03-11 19:35:13 +0700

    11/03/2558

---
+++ app/src/main/java/app/akexorcist/splashscreen/AddActivity.java
@@ -0,0 +1,15 @@
+package app.akexorcist.splashscreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Overzestful-Fhon on 3/4/2015.
+ */
+public class AddActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
---
+++ app/src/main/java/app/akexorcist/splashscreen/QRcodeMain.java
@@ -0,0 +1,21 @@
+package app.akexorcist.splashscreen;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class QRcodeMain extends Activity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.qrcode_main);
+
+
+    }
+
+}
